be89a92d8865d20d021254b1b28a46beef450a8e
Nirmal Fernando <nirmal070125@apache.org>
2014-04-22 04:20:07 +0530

    fixing ip func

--- sleep.php
+++ sleep.php
@@ -1,28 +1,10 @@
-
 <?php
 
-// current time
-echo "Going to sleep for 20s inside instance with IP: "  getIp() . "\n";
-
 // sleep for 10 seconds
-sleep(20);
+sleep(2);
 
 // wake up !
-echo "Request has been served!" . "\n";
-
-function getIp(){
-
-        $ip = $_SERVER['REMOTE_ADDR'];     
-        if($ip){
-            if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-                $ip = $_SERVER['HTTP_CLIENT_IP'];
-            } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-                $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-            }
-            return $ip;
-        }
-        // There might not be any data
-        return false;
-    }
+echo "Request is served by the instance with IP : " . $_SERVER['SERVER_ADDR'] . "\n";
 
 ?>
+
